52c862560e3629644545d73a75ed2a47ad66e0fe
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-03-07 16:35:38 +0100

    Delete creator.rb

--- creator.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby  
-require 'pp'
-require 'yaml'
-require 'erb'
-
-hash= YAML::load(File.open('data.yml').read)
-
-template = open('template.erb', 'r') {|f| f.read}
-puts ERB.new(template, 0, '>').result
