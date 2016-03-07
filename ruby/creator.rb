265abbdf1e3f8f22c3bf059e2420c16b18fa4fe9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-03-07 20:09:04 +0100

    Update creator.rb

--- ruby/creator.rb
+++ ruby/creator.rb
@@ -3,7 +3,7 @@ require 'pp'
 require 'yaml'
 require 'erb'
 
-hash= YAML::load(File.open('data.yml').read)
+hash = YAML::load(File.open('data.yml').read)
 
 template = open('template.erb', 'r') {|f| f.read}
 puts ERB.new(template, 0, '>').result
