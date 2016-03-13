8c52e4d8f8e74060b177a7f4ba4b8dac0cc5cf73
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-03-13 20:22:15 +0100

    Update and rename master.rb to creator.rb

--- ruby/creator.rb
+++ ruby/creator.rb
@@ -3,6 +3,6 @@ require 'pp'
 require 'yaml'
 require 'erb'
 
-hash = YAML::load(File.open('master.yml').read)
-template = open('master.erb', 'r') {|f| f.read}
+hash = YAML::load(File.open(ARGV[0]).read)
+template = open(ARGV[1], 'r') {|f| f.read}
 puts ERB.new(template, 0, '<>').result
