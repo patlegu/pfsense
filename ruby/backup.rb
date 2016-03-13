f5f0f9d23b1da083fecc769f440d182869107621
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-03-13 20:22:26 +0100

    Delete backup.rb

--- ruby/backup.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby  
-require 'pp'
-require 'yaml'
-require 'erb'
-
-hash = YAML::load(File.open('backup.yml').read)
-template = open('backup.erb', 'r') {|f| f.read}
-puts ERB.new(template, 0, '<>').result
