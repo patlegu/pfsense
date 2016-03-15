47d282e7c4aca2bf471dcc8176441ce72841c7d0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-03-15 16:55:18 +0100

    Update creator.rb

--- ruby/creator.rb
+++ ruby/creator.rb
@@ -5,11 +5,15 @@ hash = YAML::load(File.open(ARGV[0]).read)
 def flatten_hash(my_hash, parent=[])
   my_hash.flat_map do |key, value|
     case value
-      when Hash then flatten_hash( value, parent+[key] )
-      else line = [(parent+[key]).join('')]
-        out = "#{line}"
-        out = out[2..-3]
-        print "$config#{out} = #{value};""\n"
+      when Hash then 
+      key = "['#{key}']"  
+      flatten_hash( value, parent+[key] )
+      else 
+        key = "['#{key}']"
+        line = [(parent+[key]).join('')]
+        output = "#{line}"
+        output = output[2..-3]
+        print "$config#{output} = '#{value}';""\n"
     end
   end
 end
